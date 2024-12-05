9540100df8a0b1fcf9b714b9897012511513db2a
Ivan Shumilov <ivanshummi@gmail.com>
2024-12-05 16:07:56 +0300

    base configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,25 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Видеопродакшн полного цикла`,
+    sitesubtitle: `С 2009 года создаём профессиональные видео высокого качества`,
+    description: ``,
+    author: `Ivan Shumilov`,
+    siteUrl: `https://zemliaky.ru/`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
-};
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/icons/favicon256.png`,
+      },
+    },
+  ],
+}
