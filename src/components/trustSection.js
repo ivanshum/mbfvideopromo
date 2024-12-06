8ba9759646f0bd73b599d21d6649cad120cae4de
Ivan Shumilov <ivanshummi@gmail.com>
2024-12-06 05:48:06 +0300

    wwd and trust section base

--- src/components/trustSection.js
+++ src/components/trustSection.js
@@ -1,13 +1,48 @@
 import * as React from "react"
+import SocialGov from "../images/whatwedo/conferences.jpg"
+import MoscowCremlin from "../images/whatwedo/conferences.jpg"
+import KostromaGov from "../images/whatwedo/conferences.jpg"
+import DesignWeekend from "../images/whatwedo/conferences.jpg"
 
 const TrustSection = () => {
+  const itemsData = [
+    {
+      text: "Общественная палата РФ",
+      img: SocialGov,
+    },
+    {
+      text: "Линии Судьбы Москва Кремль",
+      img: MoscowCremlin,
+    },
+    {
+      text: "Администрация города Костромы",
+      img: KostromaGov,
+    },
+    {
+      text: "Дизайн выходные",
+      img: DesignWeekend,
+    },
+  ]
+  const Item = ({ text, img }) => (
+    <div className="flex flex-col gap-6 justify-start items-center">
+      <img className="w-full rounded-xl" src={img} alt={text} />
+      <div className="text-lg lg:text-base xl:text-lg text-slate-700 text-center">
+        {text}
+      </div>
+    </div>
+  )
   return (
-    <div className="bg-white rounded flex flex-col max-w-[600px] text-black px-8 py-4">
-      <div>
-        Получите персональное коммерческое предложение с детальной сметой в
-        течение дня
+    <div className="container mx-auto py-10">
+      <h3 className="text-3xl text-center">
+        Нам доверяют съемку событий государственные структуры
+      </h3>
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4 lg:gap-12 p-6 lg:p-12">
+        {itemsData.map((value, index) => (
+          <Item key={index} text={value.text} img={value.img} />
+        ))}
       </div>
     </div>
   )
 }
+
 export default TrustSection
--- src/components/whatWeDoSection.js
+++ src/components/whatWeDoSection.js
@@ -1,11 +1,42 @@
 import * as React from "react"
-
+import Conferences from "../images/whatwedo/conferences.jpg"
+import Seminars from "../images/whatwedo/conferences.jpg"
+import BisMeetups from "../images/whatwedo/conferences.jpg"
+import Corporate from "../images/whatwedo/conferences.jpg"
 const WhatWeDoSection = () => {
+  const itemsData = [
+    {
+      text: "Конференции",
+      img: Conferences,
+    },
+    {
+      text: "Семинары",
+      img: Seminars,
+    },
+    {
+      text: "Бизнес-встречи",
+      img: BisMeetups,
+    },
+    {
+      text: "Корпоративы компаний",
+      img: Corporate,
+    },
+  ]
+  const Item = ({ text, img }) => (
+    <div className="flex flex-col gap-6 justify-start items-center">
+      <img className="w-full rounded-xl" src={img} alt={text} />
+      <div className="text-lg lg:text-base xl:text-lg text-slate-700 text-center">
+        {text}
+      </div>
+    </div>
+  )
   return (
-    <div className="bg-white rounded flex flex-col max-w-[600px] text-black px-8 py-4">
-      <div>
-        Получите персональное коммерческое предложение с детальной сметой в
-        течение дня
+    <div className="container mx-auto py-10">
+      <h3 className="text-3xl text-center">Какие мероприятия мы снимаем?</h3>
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4 lg:gap-12 p-6 lg:p-12">
+        {itemsData.map((value, index) => (
+          <Item key={index} text={value.text} img={value.img} />
+        ))}
       </div>
     </div>
   )
